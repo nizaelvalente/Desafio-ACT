630a0e80dab4d8ebf5dbf2d4e542b543c15853a5
nizaelvalente <82558223+nizaelvalente@users.noreply.github.com>
2021-12-29 18:35:02 -0300

    feat: correção ortográfica e ajuste de cor da font

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -1,8 +1,9 @@
 
 angular.module('marvelCharacters').controller("charactersController", function ($scope, $http) {
-    $http.get("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db").then(function (data) {
+    $http.get("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db&limit=100").then(function (data) {
         $scope.characters = data.data.data.results
     });
+
     $scope.searchName = function (name) {
         let url = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db"
         if (name) {
