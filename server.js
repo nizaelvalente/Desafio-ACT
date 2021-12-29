ef65f0bb219b1869b95e3bea56e768d608cb9502
nizaelvalente <82558223+nizaelvalente@users.noreply.github.com>
2021-12-29 18:17:16 -0300

    feat: Correção da rota de busca

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/index.html'))
+})
+
+app.use(express.static(__dirname + '/src'))
+
+app.listen(process.env.PORT || 3000)
--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -2,15 +2,14 @@
 angular.module('marvelCharacters').controller("charactersController", function ($scope, $http) {
     $http.get("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db").then(function (data) {
         $scope.characters = data.data.data.results
-        
     });
-    $scope.searchName = function(name){
-        $http.get("https://gateway.marvel.com/v1/public/characters?nameStartsWith="+name+"&ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db").then(function (data) {
-            console.log(data)
-            // return data.data.data.results
+    $scope.searchName = function (name) {
+        let url = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=5a237863b3cc2061003cbbc4fe20dc06&hash=bd4b447a65ef5d6b174f87cf9db6d2db"
+        if (name) {
+            url += '&nameStartsWith=' + name;
+        }
+        $http.get(url).then(function (data) {
             $scope.characters = data.data.data.results
-    
         });
-      }
-
+    }
 });
